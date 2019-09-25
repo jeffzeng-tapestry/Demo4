ccdd98a2e5c90e0d8cfae7e62981de44ec90e85b
carti <cartigayane@gmail.com>
2019-09-25 09:34:31 -0400

    variable type change
    changed from "string" to string

--- variables.tf
+++ variables.tf
@@ -1,87 +1,87 @@
 variable "region" {
   description = "The region resources will be built in"
-  type        = "string"
+  type        = string
 }
 
 variable "environment" {
   description = "The environment the resources will be deployed in"
-  type        = "string"
+  type        = string
 }
 
 variable "role_arn" {
   description = "The role arn that is assumed to make changes"
-  type        = "string"
+  type        = string
 }
 
 variable "project" {
   description = "Project Name"
-  type        = "string"
+  type        = string
   default     = "activedirectory"
 }
 
 variable "terraform_workspace" {
   description = "The workspace to refrence for state files"
-  type        = "string"
+  type        = string
 }
 
 variable "state_bucket_name" {
   description = "The bucket in which our state files are stored."
-  type        = "string"
+  type        = string
 }
 
 variable "state_key_vpc" {
   description = "The key name for the VPC root's state file."
-  type        = "string"
+  type        = string
 }
 
 variable "state_bucket_region" {
   description = "The region that contains the state bucket"
-  type        = "string"
+  type        = string
 }
 
 variable "ec2_key_name" {
   description = "Key name to use for EC2 instances"
-  type        = "string"
+  type        = string
   default     = "authentication-prod-key"
 }
 
 variable "ec2_iam_instance_profile" {
   description = "IAM Instance Profile to attach to EC2 instances"
-  type        = "string"
+  type        = string
   default     = "TEST_SSM_ROLE"
 }
 
 variable "subnet_usawipwcohdc01" {
   description = "Which subnet to launch usawipwcohdc01 in to"
-  type        = "string"
+  type        = string
 }
 
 variable "subnet_usawipwglbdc01" {
   description = "Which subnet to launch usawipwglbdc01 in to"
-  type        = "string"
+  type        = string
 }
 
 variable "subnet_usawipwglbdc02" {
   description = "Which subnet to launch usawipwglbdc02 in to"
-  type        = "string"
+  type        = string
 }
 
 variable "kms_key_id" {
   description = "KMS key to encrypt EBS volumes with"
-  type        = "string"
+  type        = string
 }
 
 variable "global_dc_1_name" {
   description = "Name of global DC 1"
-  type        = "string"
+  type        = string
 }
 
 variable "global_dc_2_name" {
   description = "Name of global DC 2"
-  type        = "string"
+  type        = string
 }
 
 variable "coach_dc_1_name" {
   description = "Name of coach DC 1"
-  type        = "string"
+  type        = string
 }
